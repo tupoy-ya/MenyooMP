1e8f3088dae27e126f2918f99ef42ca2a3af4acb
tupoy-ya <tupoy-ya@users.noreply.github.com>
2022-11-29 12:43:58 +0500

    feat(pch): Added precompiled header. refactor(CMakeLists.txt): Added gtav_classes.

---
+++ Solution/scripts/gtav-classes.cmake
@@ -0,0 +1,14 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    gtav_classes
+    GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
+    GIT_TAG        257115997e5ecea9fafbc820a746d9c6f5889e57
+    GIT_PROGRESS TRUE
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+)
+message("GTAV-Classes")
+if(NOT gtav_classes_POPULATED)
+    FetchContent_Populate(gtav_classes)
+endif()
---
+++ Solution/source/common.h
@@ -0,0 +1,20 @@
+#ifndef COMMON_INC
+#define COMMON_INC
+
+#include <windows.h>
+
+#include <vector>
+#include <string>
+#include <array>
+#include <utility>
+
+
+#include "Menu/Menu.h"
+#include "Menu/Routine.h"
+#include "Memory/GTAmemory.h"
+#include "Natives/natives2.h"
+#include "Util/GTAmath.h"
+#include "Scripting/GTAped.h"
+#include "Scripting/Game.h"
+
+#endif
